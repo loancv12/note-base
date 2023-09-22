906d5473b3796a5d6e77a20910578f08de3f4b2f
loancv12 <loancv12@gmail.com>
2023-09-22 17:28:32 +0700

    check devtool

--- src/app/store.js
+++ src/app/store.js
@@ -11,7 +11,7 @@ export const store = configureStore({
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(apiSlice.middleware),
-  // devTools: true,
+  devTools: false,
 });
 
 // refresh data at some type of interval
